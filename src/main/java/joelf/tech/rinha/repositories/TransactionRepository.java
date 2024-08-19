168c804f25ac2676ad685016ef34e171bb4178e7
Joel Filho <joelfilho02812@gmail.com>
2024-08-19 18:17:20 -0300

    feat: add transaction repository

---
+++ src/main/java/joelf/tech/rinha/repositories/TransactionRepository.java
@@ -0,0 +1,20 @@
+package joelf.tech.rinha.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import joelf.tech.rinha.models.Transaction;
+
+import java.util.*;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT tt.valor, tt.tipo, tt.descricao, tt.realizado_em FROM tb_transacao tt "
+            + "INNER JOIN tb_client tc ON tc.id = tt.client_id "
+            + "WHERE tt.client_id = :id "
+            + "ORDER BY tt.realizado_em DESC "
+            + "LIMIT 10")
+    public List<Transaction> getTransactionByClientId(Long id);
+}
