50e3108ad29555d798e8c12bef966275c5d7aeb0
Joel Filho <joelfilho02812@gmail.com>
2024-08-19 14:53:34 -0300

    chore: init project

---
+++ src/test/java/joelf/tech/rinha/RinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package joelf.tech.rinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
